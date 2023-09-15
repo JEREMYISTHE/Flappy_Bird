2b7a42e6969760887911cbb0af4646a873cf5c07
jerem <jeremyturtel@gmail.com>
2023-09-15 16:30:20 -0400

    print"bello"

--- flappy real game.py
+++ flappy real game.py
@@ -1,4 +1,5 @@
-import random  # For generating random numbers
+print ("hello")
+ import random  # For generating random numbers
 import sys  # We will use sys.exit to exit the program
 import pygame
 from pygame.locals import *  # Basic pygame imports
